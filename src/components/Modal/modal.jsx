0f7ece93eddd6832db98fd17a8e671266d370228
Yuriy Chernyakov <nitro_06@ukr.net>
2023-03-22 15:52:24 +0200

    test v2.0

--- src/components/Modal/modal.jsx
+++ src/components/Modal/modal.jsx
@@ -1,25 +1,9 @@
 import css from '../styles.module.css';
 import PropTypes from 'prop-types';
 
-export const Modal = ({ item }) => {
+export const Modal = ({ item}) => {
   
-  // componentDidMount() {
-  //   window.addEventListener('keydown', this.handleEscpClick);
-  // }
-  // componentWillUnmount() {
-  //   window.removeEventListener('keydown', this.handleEscpClick);
-  // }
-  // handleEscpClick = e => {
-  //   if (e.code === 'Escape') {
-  //     this.props.closeModal();
-  //   }
-  // };
-  // onClick = e => {
-  //   if (e.target === e.currentTarget) {
-  //     this.props.closeModal();
-  //   }
-  // };
-  return (<div className={css.overlay} tabindex='-1'>
+  return (<div className={css.overlay}>
     <div className={css.modal}>
       <img src={item.largeImageURL} alt="" />
     </div>
@@ -28,5 +12,5 @@ export const Modal = ({ item }) => {
 }
 
 Modal.propTypes = {
-  item: PropTypes.string.isRequired,
+  item: PropTypes.string,
 };
